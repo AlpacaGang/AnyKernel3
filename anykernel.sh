47da3718b6d395a7827ee11b16e24ed742bc2f2c
FedorShatokhin2005 <groza01i@gmail.com>
2020-06-24 05:42:35 +0000

    AK3: repack ramdisk after dump_boot
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -35,10 +35,10 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-repack_ramdisk;
-
 ## AnyKernel install
 dump_boot;
+
+repack_ramdisk;
 
 write_boot;
 ## end install
